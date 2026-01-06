8530f71ef6622af892dc9f20841b588a0c63a23d
何岸 <1994104357@qq.com>
2026-01-06 22:22:14 +0800

    应用模块完成，应用就是零代码生成的AI应用，大致是3方面的能力 应用管理 应用生成 应用部署

---
+++ src/main/java/com/noncodebacked/Mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.noncodebacked.Mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.noncodebacked.Model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
